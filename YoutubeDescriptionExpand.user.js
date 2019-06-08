b88a05496e230e4830dab2d3de8d869047736a54
TheBestPessimist <cristian@tbp.land>
2019-06-08 12:28:34 +0300

    Create YoutubeDescriptionExpand.user.js

---
+++ YoutubeDescriptionExpand.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name            YouTube Description Expand
+// @description     Expand YouTube description section
+// @version         1.0
+// @namespace       https://git.tbp.land/
+// @match           *://*youtube.com/watch*
+// @compatible      firefox
+// @run-at          document-idle
+// ==/UserScript==
+
+
+function yt(){
+  document.querySelector("#more").click();
+}
+
+document.addEventListener('readystatechange', () => setTimeout(yt, 2000));
