4ebb998907e26d7add7c8a03d47c0c9afb00fc4a
TheBestPessimist <cristian@tbp.land>
2019-06-10 13:03:52 +0300

    Add author

--- YoutubeDescriptionExpand.user.js
+++ YoutubeDescriptionExpand.user.js
@@ -2,6 +2,7 @@
 // @name            YouTube Description Expand
 // @description     Expand YouTube description section
 // @version         1.1
+// @author          TheBestPessimist
 // @namespace       https://git.tbp.land/
 // @match           *://*.youtube.com/*
 // @compatible      firefox
