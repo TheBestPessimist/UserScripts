bb6c3c3d6659626d0ad83557509f1adf1518e50e
TheBestPessimist <cristian@tbp.land>
2021-12-01 14:14:33 +0200

    Update wait-for-elements cdn

--- youtube-description-autoexpand/youtube-description-autoexpand.user.js
+++ youtube-description-autoexpand/youtube-description-autoexpand.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name            YouTube Description Autoexpand
 // @description     Expand YouTube description section automatically
-// @version         1.4.1
+// @version         1.4.2
 // @author          TheBestPessimist
 // @namespace       https://git.tbp.land/
 // @match           *://*.youtube.com/*
-// @require         https://gitcdn.xyz/repo/fuzetsu/userscripts/b38eabf72c20fa3cf7da84ecd2cefe0d4a2116be/wait-for-elements/wait-for-elements.js
+// @require         https://ghcdn.rawgit.org/fuzetsu/userscripts/3d35399f95c3b6796210f5594bcbf243297f8f96/wait-for-elements/wait-for-elements.js
 // ==/UserScript==
 /**
  * Note: Using the function `waitForElems` from the required library above
@@ -31,11 +31,3 @@ waitForElems({
         btn.click();
     }
 });
-
-
-
-
-
-
-
-
