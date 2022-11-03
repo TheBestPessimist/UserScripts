f3fd5c4aecb7d6de8998d99d7f7563e2af8b00fc
TheBestPessimist <cristian@tbp.land>
2022-11-03 10:36:16 +0200

    Update Youtube auto dislike

--- youtube-auto-dislike/youtube-auto-dislike.user.js
+++ youtube-auto-dislike/youtube-auto-dislike.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            YouTube Auto Dislike
 // @description     Dislike every YouTube video. This is my rebuttal for YouTube hiding the Dislike count.
-// @version         1.0.1
+// @version         1.0.2
 // @author          TheBestPessimist
 // @namespace       https://git.tbp.land/
 // @match           *://*.youtube.com/*
@@ -24,11 +24,10 @@ const util = {
 const SCRIPT_NAME = "YouTube Auto Dislike";
 
 const CSS = {
-    like: "ytd-video-primary-info-renderer #top-level-buttons-computed > ytd-toggle-button-renderer:nth-child(1)",
-    dislike: "ytd-video-primary-info-renderer #top-level-buttons-computed > ytd-toggle-button-renderer:nth-child(2)",
-    buttonClicked: "style-default-active"
+    dislike: "#above-the-fold #menu #segmented-dislike-button .yt-spec-button-shape-next",
+    likeButtonClicked: '#above-the-fold #menu #segmented-like-button .yt-spec-button-shape-next[aria-pressed="true"]',
+    dislikeButtonClicked: '#above-the-fold #menu #segmented-dislike-button .yt-spec-button-shape-next[aria-pressed="true"]',
 }
-
 
 waitForUrl(
     () => true, // match any url
@@ -37,7 +36,7 @@ waitForUrl(
         // Not sure why, but it looks as if the html is not yet changed even though the url is.
         setTimeout(
             () => waitForElems({
-                sel: CSS.like,
+                sel: CSS.dislike,
                 onmatch: dislike,
             }),
             1000
@@ -47,23 +46,30 @@ waitForUrl(
 
 
 function dislike(btn) {
-    util.log("begin debug")
-    util.log(btn)
+    util.log("begin debug");
 
-    const likeButton = document.querySelector(CSS.like)
-    const dislikeButton = document.querySelector(CSS.dislike)
+    const dislikeButton = document.querySelector(CSS.dislike);
+    const likeButtonClicked = document.querySelector(CSS.likeButtonClicked);
+    const dislikeButtonClicked = document.querySelector(CSS.dislikeButtonClicked);
 
-    if (likeButton.classList.contains(CSS.buttonClicked)) {
-        util.log("Video is Liked by user => Nothing to do here. Will not dislike.")
+    util.log("dislike button:");
+    util.log(dislikeButton);
+    util.log("likeButtonClicked");
+    util.log(likeButtonClicked);
+    util.log("dislikeButtonClicked");
+    util.log(dislikeButtonClicked);
+
+    if (likeButtonClicked) {
+        util.log("Video is Liked by user. Will not dislike. => Nothing to do here.");
         return
     }
 
-    if (dislikeButton.classList.contains(CSS.buttonClicked)) {
-        util.log("Video is already Disliked => Nothing to do here.")
+    if (dislikeButtonClicked) {
+        util.log("Video is already Disliked => Nothing to do here.");
         return
     }
 
-    util.log("Will Dislike video.")
-    dislikeButton.click()
-    util.log("end debug")
+    util.log("Will Dislike video.");
+    dislikeButton.click();
+    util.log("end debug");
 }
