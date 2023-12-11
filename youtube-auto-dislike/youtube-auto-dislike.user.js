aacdf3743163583d82a1c2e90f059412c42a7197
TheBestPessimist <cristian+gitpush@tbp.land>
2023-12-11 17:09:33 +0200

    FUCK JAVASCRIPT

--- youtube-auto-dislike/youtube-auto-dislike.user.js
+++ youtube-auto-dislike/youtube-auto-dislike.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            YouTube Auto Dislike
 // @description     Dislike every YouTube video. This is my rebuttal for YouTube hiding the Dislike count.
-// @version         1.0.7
+// @version         1.0.8
 // @author          TheBestPessimist
 // @namespace       https://git.tbp.land/
 // @match           *://*.youtube.com/*
@@ -35,9 +35,8 @@ waitForElems({
 })
 
 async function dislike() {
-    util.log("before sleep");
-    // need to wait here a little for the elements to load. how do i do that besides a sleep?
-    await new Promise(resolve => setTimeout(resolve, 5000));
+    // need to wait here a little for the elements to load, else `ariaPressed` is null.
+    await new Promise(resolve => setTimeout(resolve, 1000));
 
     const likeButton = document.querySelector(CSS.likeButton);
     const dislikeButton = document.querySelector(CSS.dislikeButton);
@@ -45,30 +44,35 @@ async function dislike() {
     /*
      FUCK JAVASCRIPT
      FUCK YOU WHOEVER INVENTED TRUTHY AND FALSY
-     FUCK YOU WHOEVER THINGS IF NORMAL TO NOT HAVE TRUE AND FALSE AS PROPER TYPES BUT AS STRINGS
+     FUCK YOU WHOEVER THINGS IF NORMAL TO NOT HAVE TRUE AND FALSE AS PROPER TYPES BUT AS FUCKING STRINGS
      FUCK YOU
      FUCK YOU
      FUCK YOU
      FUCKING FUCK YOU, YOU FUCKING GARBAGE LANGUAGE
+     FUCK YOUR FUCKING APOLOGISTS
      WHAT THE FUCKING FUCK IS FUCKING `=== 'true'`??? WHY NOT SIMPLY `== true` LIKE ANY FUCKING SANE LANGUAGE DOES?
      FUCK YOU
-     FUCK YOU WHOEVER THINKS FUCKING `== 'true'`, `=== 'true'`, `== true`, `=== true` IS RIGHT
+     FUCK TYPE COERCION
+     FUCK YOU WHOEVER THINKS FUCKING `== 'true'`, `=== 'true'`, `== true`, `=== true` IS 'RIGHT' AND 'SANE' AND 'EASY'
      ALL THIS FUCKING SHIT IS RETARDED
-     */
-    const likeButtonClicked = likeButton.ariaPressed == 'true'
-    const dislikeButtonClicked = dislikeButton.ariaPressed == 'true'
 
-    util.log(`like button already clicked: ${likeButtonClicked}`);
+     const likeButtonClicked = likeButton.ariaPressed == 'true'
+     const dislikeButtonClicked = dislikeButton.ariaPressed == 'true'
+     */
+
+    util.log(`like button ariaPressed value: ${likeButton.ariaPressed}`);
     util.log(likeButton);
-    util.log(`dislike button already clicked: ${dislikeButtonClicked}`);
+    util.log(`dislike button ariaPressed value: ${dislikeButton.ariaPressed}`);
     util.log(dislikeButton);
 
-    if (likeButtonClicked) {
+    // noinspection EqualityComparisonWithCoercionJS
+    if (likeButton.ariaPressed == 'true') {
         util.log("Video is Liked by user. Will not dislike. => Nothing to do here.");
         return
     }
 
-    if (dislikeButtonClicked) {
+    // noinspection EqualityComparisonWithCoercionJS
+    if (dislikeButton.ariaPressed == 'true') {
         util.log("Video is already Disliked => Nothing to do here.");
         return
     }
