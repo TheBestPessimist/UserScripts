f6cc8330836ba5014932145af24306e0f551b3ca
TheBestPessimist <cristian+gitpush@tbp.land>
2024-12-12 15:03:42 +0200

    Video Playback Speed

--- video-speed/video-playback-speed.js
+++ video-speed/video-playback-speed.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Video Playback Speed
 // @description  Change video playback speed with keys `[`, `]`, `\`
-// @version      1.0.2
+// @version      1.0.3
 // @author       TheBestPessimist
 // @run-at       document-end
 // @namespace    https://git.tbp.land/
@@ -43,10 +43,17 @@ const vsc = {
         // i like pretty numbers :^)
         newSpeed = newSpeed.toFixed(2);
 
+        // youtube gives errors for anything outside this range
+        newSpeed = clamp(newSpeed, 0.1, 15)
+
         vsc.log(`Speed: ${currentSpeed} -> ${newSpeed}`);
         vsc.getVideo().playbackRate = newSpeed;
     }
 };
+
+function clamp(number, min, max) {
+    return Math.max(min, Math.min(number, max));
+}
 
 document.body.addEventListener("keydown", vsc.ev_keydown);
 vsc.log(`loaded`);
