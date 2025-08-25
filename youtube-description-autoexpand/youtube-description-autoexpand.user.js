79a07141473e090cfd18d3ae122b066955337eba
TheBestPessimist <cristian@tbp.land>
2025-08-25 15:11:38 +0300

    Update youtube description tag

--- youtube-description-autoexpand/youtube-description-autoexpand.user.js
+++ youtube-description-autoexpand/youtube-description-autoexpand.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            YouTube Description Autoexpand
 // @description     Expand YouTube description section automatically
-// @version         1.4.9
+// @version         1.4.10
 // @author          TheBestPessimist
 // @namespace       https://git.tbp.land/
 // @match           *://*.youtube.com/*
@@ -17,7 +17,7 @@ const util = {
 
 const SCRIPT_NAME = 'YouTube Description Autoexpand';
 
-const showMoreButtonId = 'tp-yt-paper-button#expand';
+const showMoreButtonId = '#description #expand';
 
 waitForUrl(
     () => true, // match any url
